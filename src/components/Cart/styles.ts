c3961275d52c445ad648d8323301567e87564244
Cleiton Alves <cleiton_jc@yahoo.com.br>
2021-12-24 14:24:31 -0300

    Cart component

---
+++ src/components/Cart/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const CartContainer = styled.div`
+  position: relative;
+  height: 26px;
+
+  > img {
+    height: 26px;
+  }
+`
+
+export const NumberOfItems = styled.div`
+  position: absolute;
+  right: -8px;
+  top: -8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${props => props.theme.colors.grayDark};
+  color: ${props => props.theme.colors.white};
+  width: 17px;
+  height: 17px;
+  font-size: ${props => props.theme.fontSize.extraSmall};
+  border-radius: 50%;
+`
